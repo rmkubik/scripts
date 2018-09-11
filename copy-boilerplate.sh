88e8b17c116a50762367956fec36abebaa8bdbf3
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 10:49:49 -0700

    Update to use rsync instead of cp to ignore node_modules and .git instead of retroactively deleting them

--- copy-boilerplate.sh
+++ copy-boilerplate.sh
@@ -1,9 +1,8 @@
 # $1 == source directory path, first parameter
 # $2 == destination directory path, second parameter
-cp -r $1 $2
+# Can use -n flag on rsync to verify copy before executing
+rsync -av --progress $1/* $2 --exclude node_modules --exclude .git
 cd $2
-rm -rf node_modules
-rm -rf .git
 git init
 git add .
-git commit -m 'initial commit'"
+git commit -m "Initial commit"
