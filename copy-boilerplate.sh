75ee23890091ae2ed05f34d2b0952c19bbc56d53
Ryan Kubik <ryan.kubik@nike.com>
2018-09-27 23:22:17 -0700

    Update copy-boilerplate

--- copy-boilerplate.sh
+++ copy-boilerplate.sh
@@ -1,8 +1,13 @@
 # $1 == source directory path, first parameter
 # $2 == destination directory path, second parameter
 # Can use -n flag on rsync to verify copy before executing
-rsync -av --progress $1/* $2 --exclude node_modules --exclude .git
+rsync -a --progress $1/ $2/ --exclude node_modules/ --exclude .git/
 cd $2
 git init
 git add .
 git commit -m "Initial commit"
+echo -n "Current Author: "
+cat package.json | jq .author
+echo -n "Current License: "
+cat package.json | jq .license
+echo "Delete README.md?"
