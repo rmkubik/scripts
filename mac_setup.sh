2a89a55c3ebc6cecdba3219e302d250189ac3957
Ryan Kubik <rmkubik@me.com>
2019-12-07 00:20:28 -0800

    Change git commit to cm

--- mac_setup.sh
+++ mac_setup.sh
@@ -60,7 +60,7 @@ git config --global user.email $1
 git config --global color.ui "true"
 git config --global push.default simple
 git config --global alias.co checkout
-git config --global alias.ci commit
+git config --global alias.cm commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global credential.helper osxkeychain
