22e9b413741f990a60441391b8ea258265b83ff7
Ryan Kubik <rmkubik@me.com>
2020-05-17 15:02:48 -0700

    Count days worked git

---
+++ count_days_worked.js
@@ -0,0 +1,84 @@
+const fs = require("fs").promises;
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+// git log --author="Ryan Kubik" --format="%cD" > commits.txt
+
+class DateMap {
+  constructor() {
+    this.dates = {};
+  }
+
+  add(date) {
+    // strip time
+    date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+    if (!this.contains(date)) {
+      this.dates[date] = 0;
+    }
+
+    this.dates[date] += 1;
+  }
+
+  contains(date) {
+    return Object.keys(this.dates).some((existingDate) =>
+      DateMap.compare(date, new Date(existingDate))
+    );
+  }
+
+  static compare(a, b) {
+    return (
+      a.getDate() === b.getDate() &&
+      a.getMonth() === b.getMonth() &&
+      a.getFullYear() === b.getFullYear()
+    );
+  }
+
+  toString() {
+    return Object.entries(this.dates)
+      .map(([date, commits]) => {
+        const formatter = new Intl.DateTimeFormat("en", {
+          year: "numeric",
+          month: "short",
+          day: "2-digit",
+        });
+
+        return `    ${formatter.format(new Date(date))}: ${commits} commits`;
+      })
+      .join("\n");
+  }
+}
+
+// git log --author="Ryan Kubik" --format="%cD" > commits.txt
+async function read() {
+  const { stdout, stderr } = await exec(
+    `git log --author="${process.argv[2]}" --format="%cD" < /dev/tty`
+  );
+
+  if (stderr) {
+    throw stderr;
+  }
+
+  const dates = new DateMap();
+  const commits = stdout; // await fs.readFile("./commits.txt", "utf-8");
+
+  commits.split("\n").forEach((commitDate) => {
+    if (commitDate.length > 0) {
+      const date = new Date(commitDate);
+
+      dates.add(date);
+    }
+  });
+
+  console.log(`
+    For Author: ${process.argv[2]}
+
+    Total Days: ${Object.keys(dates.dates).length}
+    Total Commits: ${Object.values(dates.dates).reduce(
+      (count, commits) => count + commits,
+      0
+    )}
+${process.argv[3] === "showDates" ? `\n${dates.toString()}\n` : ""}`);
+}
+
+read();
